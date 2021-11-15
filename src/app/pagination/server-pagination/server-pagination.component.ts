511d561ef5ddc5fb23fbae5c731afc31f05c9a5d
Anand raja <meetanandns@gmail.com>
2021-11-15 17:28:38 +0000

    custom pagination works

--- src/app/pagination/server-pagination/server-pagination.component.ts
+++ src/app/pagination/server-pagination/server-pagination.component.ts
@@ -14,12 +14,17 @@ export class ServerPaginationComponent implements OnInit, AfterViewInit {
 
   dataSource: ServerPaginationJsonDatasource;
   displayedColumns = ['id', 'userId', 'title', 'body'];
+  pageRange: number;
+  pages;
+  activePage = 1;
+  defaultPageSize = 5;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private jsonServ: JsonPlaceholderService) { }
 
   ngOnInit(): void {
+    this.getPageRangeArray();
     this.dataSource = new ServerPaginationJsonDatasource(this.jsonServ);
     this.dataSource.loadJsonData(0, 5)
   }
@@ -30,6 +35,55 @@ export class ServerPaginationComponent implements OnInit, AfterViewInit {
       // console.log(this.paginator.pageSize);
       this.dataSource.loadJsonData(this.paginator.pageIndex, this.paginator.pageSize);
     });
+  }
+
+  // to generate page number
+  getPageRangeArray(pageRange = 10) {
+    this.pageRange = pageRange;
+    const pageVar = Array.from(Array(pageRange + 1).keys()).slice(1);
+    if (this.pageRange > this.defaultPageSize + 1) {
+      this.pages = [1, 2, 3, 4, 5, '...', this.pageRange];
+    } else {
+      this.pages = pageVar;
+    }
+  }
+
+  getTableData() {
+    this.dataSource.loadJsonData(this.paginator.pageIndex, this.paginator.pageSize);
+  }
+
+  appFirstPage(page: number): void {
+    this.paginator.firstPage();
+    this.activePage = page;
+    this.getTableData();
+  }
+
+  getCurrentpage(): number {
+    return this.activePage;
+  }
+
+  appPreviousPage(page: number): void {
+    this.paginator.previousPage();
+    this.activePage = page;
+    this.getTableData();
+  }
+
+  appCurrentPage(page: number): void {
+    this.paginator.pageIndex = page - 1;
+    this.activePage = page;
+    this.getTableData();
+  }
+
+  appNextPage(page: number): void {
+    this.paginator.nextPage();
+    this.activePage = page;
+    this.getTableData();
+  }
+
+  appLastPage(page: number): void {
+    this.paginator.lastPage();
+    this.activePage = page;
+    this.getTableData();
   }
 
 }
