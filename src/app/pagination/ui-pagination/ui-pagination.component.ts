d89c9715f19493e19e8b8bb4336b8f9bb2f43928
Anand raja <meetanandns@gmail.com>
2021-12-05 07:59:05 +0000

    UI side basic pagination works

--- src/app/pagination/ui-pagination/ui-pagination.component.ts
+++ src/app/pagination/ui-pagination/ui-pagination.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UiPaginationComponent implements OnInit {
 
+  title = "Angular 2 simple pagination";
+
+  curPage = 1;
+  pageSize = 3; 
+
+  list = [
+    {name:'Prashobh',age:'25'},
+    {name:'Abraham',age:'35'},
+    {name:'Anil',age:'40'},
+    {name:'Sam',age:'40'},
+    {name:'Philip',age:'40'},
+    {name:'Bal',age:'40'},
+    {name:'Anu',age:'20'}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  numberOfPages(){
+    return Math.ceil(this.list.length / this.pageSize);
+  };
 
 }
